097f134dd8872ab7bf156289684ea74f8f685e0c
CaeVye <caevye@posteo.de>
2017-06-04 23:53:58 +0200

    Array size error now correctly fixed, further logs added

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -10,7 +10,7 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_numbers);
-        String[] words = new String[11];
+        String[] words = new String[10];
         words[0] = "one";
         words[1] = "two";
         words[2] = "three";
@@ -19,10 +19,18 @@ public class NumbersActivity extends AppCompatActivity {
         words[5] = "six";
         words[6] = "seven";
         words[7] = "eight";
-        words[9] = "nine";
-        words[10] = "ten";
+        words[8] = "nine";
+        words[9] = "ten";
 
        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
+       Log.v("NumbersActivity", "Word at index 2: " + words[2]);
+       Log.v("NumbersActivity", "Word at index 3: " + words[3]);
+       Log.v("NumbersActivity", "Word at index 4: " + words[4]);
+       Log.v("NumbersActivity", "Word at index 5: " + words[5]);
+       Log.v("NumbersActivity", "Word at index 6: " + words[6]);
+       Log.v("NumbersActivity", "Word at index 7: " + words[7]);
+       Log.v("NumbersActivity", "Word at index 8: " + words[8]);
+       Log.v("NumbersActivity", "Word at index 9: " + words[9]);
     }
 }
